c6a13d6f0f0f519e92b3a804b3c791f8ed44a46c
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-11-05 10:51:56 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-    return(
-      <h1>Hello World</h1>
-    );
-}
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
+};
 
 export default Home;
