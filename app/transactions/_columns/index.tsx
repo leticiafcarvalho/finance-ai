bca44a3f7b0f1c0f6b27c5f42940366ae8675161
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-11-07 10:18:53 -0300

    feat: ensure transaction can be update

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -5,12 +5,10 @@ import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
 import { Button } from "@/app/_components/ui/button";
 import { TrashIcon } from "lucide-react";
-
 import {
   TRANSACTION_CATEGORY_LABELS,
   TRANSACTION_PAYMENT_METHOD_LABELS,
 } from "@/app/_constants/transactions";
-
 import EditTransactionButton from "../_components/edit-transaction-button";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
