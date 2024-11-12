a583c78d622efba69c4863e33fa137acdc84fd09
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-11-12 18:03:18 -0300

    fix: const nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/app", // Somente se precisar de um caminho base específico
-  trailingSlash: true, // Opcional: adiciona uma barra ao final das URLs
 };
 
 export default nextConfig;
